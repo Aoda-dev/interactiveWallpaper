43b4f34316dd10555dd3830d0fbb56411671b524
Tolegen Kulseitov <hinokamiaoda@gmail.com>
2022-07-09 02:41:32 +0600

    added time

--- src/mainLoad.js
+++ src/mainLoad.js
@@ -4,18 +4,25 @@ import BIRDS from 'vanta/dist/vanta.birds.min'
 export const mainLoad = () => {
 	return new Promise((resolve) => {
 		document.querySelector('#app').innerHTML = `
-			<div class="origin-container overflow-hidden relative w-screen h-screen bg-slate-800 bg-blend-overlay bg-first bg-cover bg-center bg-fixed flex flex-col items-center justify-center text-white">
+			<div class="origin-container overflow-hidden relative w-screen h-screen bg-slate-800 bg-blend-overlay bg-second bg-cover bg-center bg-fixed flex flex-col items-center justify-center text-white">
 				<div class="animate-top w-screen absolute h-1/2 bg-black text-black/0">empty</div>
 				<div class="animate-bot w-screen absolute h-1/2 bg-black text-black/0">empty</div>
 
 				<div class="vanta-anim w-screen h-screen space-y-2 flex flex-col items-center justify-center text-center font-main select-none ">
 					<div class="text-9xl">
-						<span>02:59:42</span>
+						<span class="time">02:59:42</span>
 					</div>
-					<span class="max-w-xl">First ask yourself who you want to be, and then do what you have to do</span>
+					<span class="text-lg text-white/90">First ask yourself who you want to be, and then do what you have to do</span>
 				</div>
 			</div>
 		`
+
+		const timeWrapper = document.querySelector('.time')
+
+		setInterval(() => {
+			const date = new Date()
+			timeWrapper.innerText = `${date.toLocaleTimeString()}`
+		}, 1000)
 
 		anime({
 			easing: 'easeOutExpo',
